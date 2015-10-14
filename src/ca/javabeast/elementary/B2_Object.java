668cd179b7c66c7941779c9ce3ccb6e10f60d939
Alpenliebe <alpseinstein@gmail.com>
2015-10-14 17:53:34 -0400

    beast 02 : Object beast(abstract,interface,inner class)

--- src/ca/javabeast/elementary/B2_Object.java
+++ src/ca/javabeast/elementary/B2_Object.java
@@ -20,18 +20,74 @@ package ca.javabeast.elementary;
  * @author Alpenliebe <alpseinstein@gmail.com>
  */
 public class B2_Object {
+    
+    class BA{  
+        int a = 10;  
+        void b(){  
+            System.out.println("this is A.b()!");  
+        }  
+    } 
+    
     /**
      * 
      * @param args 
      */     
     public static void main(String[] args){
        //Build b = new Build();
-        System.out.println(a("egg", ":niu", ":baby!")); 
+        System.out.println(a("egg", ":niu", ":baby!"));
+        Hero h = new Hero();  
+        t(h);  
+        u(h);  
+        v(h);  
+        w(h);
+        
+        B2_Object bo = new B2_Object();  
+        B2_Object.BA aa = bo.new BA();  
+        aa.b();
+        
+        /*non-static inner class*/  
+        InnerClass ic = new InnerClass();  
+        InnerClass.A a = ic.new A();  
+        a.a();  
+          
+        /*static inner class*/  
+        InnerClass.C c = new InnerClass.C();  
+        c.c(); 
+        
+        //extends inner class
+        CC cc = new CC();  
+        takesA(cc);  
+        takesB(cc.makeB()); 
     }
+    
+    static void takesA(AA a){}  
+    static void takesB(BB b){}
+    
     public static String a(String... value) {  
         return "welcome to you :" + value[0] + value[1] + value[2];  
     }
+    
+    public static void t(CanFight x){x.fight();}  
+      
+    public static void u(CanSwim x){x.swim();}  
+      
+    public static void v(CanFly x){x.fly();}  
+      
+    public static void w(ActionCharacter x){x.fight();} 
 }
+
+class InnerClass {  
+    class A{  
+        void a(){  
+            System.out.println("this is A.a()!");  
+        }  
+    }  
+    static class C{  
+        void c(){  
+            System.out.println("this is C.c()!");  
+        }  
+    }  
+} 
 
 class Person {  
     
@@ -65,4 +121,34 @@ class Build {
   
     Person p3 = new Person(3);  
   
+} 
+
+
+interface CanFight {void fight();}  
+interface CanFly {void fly();}  
+interface CanSwim {void swim();}  
+class ActionCharacter {public void fight(){}}  
+class Hero extends ActionCharacter implements CanFight, CanFly, CanSwim {  
+  
+    @Override  
+    public void swim() {}  
+  
+    @Override  
+    public void fly() { }  
+  
+} 
+
+//extends inner class
+class AA{  
+      
+}  
+abstract class BB{  
+      
+}  
+class CC extends AA{  
+    BB makeB(){  
+        return new BB(){  
+              
+        };  
+    }  
 } 
