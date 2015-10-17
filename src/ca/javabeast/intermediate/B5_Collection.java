f90f2255c8cb8c3b71b15032fa4d0ebf411b3dfa
Alpenliebe <alpseinstein@gmail.com>
2015-10-17 16:44:44 -0400

    beast 05 : Collection(Collections)

--- src/ca/javabeast/intermediate/B5_Collection.java
+++ src/ca/javabeast/intermediate/B5_Collection.java
@@ -15,7 +15,11 @@
  */
 package ca.javabeast.intermediate;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,6 +32,9 @@ public class B5_Collection {
     public static void main(String[] args){
         int [] a = {2,3,2,2,1,4,2,2,2,7,9,6,2,2,3,1,0}; 
         printN(a);
+        testCollections();
+        sorted();
+        rotate();
     }
     
     /**
@@ -45,5 +52,74 @@ public class B5_Collection {
             if (map.get(key) >= array.length/2)
                 System.out.println(key+":"+map.get(key));
         }
+    }
+    
+    public static void testCollections(){
+        List<Integer> list = new ArrayList<Integer>();  
+        list.add(1);  
+        list.add(2);  
+          
+        for (Integer integer : list) {  
+            System.out.println(integer);  
+        }  
+        /*find max*/  
+        int max = Collections.max(list);  
+        System.out.println("max:"+max);  
+          
+        /*replace elements with e*/  
+        Collections.fill(list, 6);  
+        System.out.println("after filled:");  
+        for (Integer integer : list) {  
+            System.out.println(integer);  
+        }  
+          
+        /*find the frequency of the element*/  
+        int count = Collections.frequency(list, 6);  
+        System.out.println("frequency:"+count);
+    }
+    
+    public static void sorted(){
+        List<Integer> list = new ArrayList<Integer>();  
+        list.add(5);  
+        list.add(2);  
+        list.add(1);  
+        list.add(9);  
+        list.add(0);  
+          
+        System.out.println("before sort:");  
+        for (Integer integer : list) {  
+            System.out.print(integer+" ");  
+        }  
+        System.out.println();  
+          
+        /*sort*/  
+        Collections.sort(list);  
+          
+        System.out.println("after sort");  
+        for (Integer integer : list) {  
+            System.out.print(integer+" ");  
+        }
+    }
+    
+    public static void rotate(){
+        List<Integer> list = new ArrayList<Integer>();  
+        list.add(5);  
+        list.add(2);  
+        list.add(1);  
+        list.add(9);  
+        list.add(0);  
+          
+        System.out.println("original seq:");  
+        for (Integer integer : list) {  
+            System.out.print(integer+" ");  
+        }  
+        System.out.println();  
+          
+        /*rotate according the distance*/  
+        Collections.rotate(list, -1);  
+        System.out.println("after rotate:");  
+        for (Integer integer : list) {  
+            System.out.print(integer+" ");  
+        } 
     }
 }
