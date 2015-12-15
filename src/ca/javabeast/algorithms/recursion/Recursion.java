8dfca60000f5b6e5b16496c760735eec0c9b58dc
alpenliebe <alpseinstein@gmail.com>
2015-12-14 20:21:09 -0500

    Recursion-Binary Search

--- src/ca/javabeast/algorithms/recursion/Recursion.java
+++ src/ca/javabeast/algorithms/recursion/Recursion.java
@@ -21,31 +21,51 @@ package ca.javabeast.algorithms.recursion;
  * @author alpenliebe <alpseinstein@gmail.com>
  */
 public class Recursion {
-    public static int findTarget(int[] arr,int n){
-        int left=0,right=arr.length-1,i=0;
-        while(left>=right){
+    public static int findTarget (int[] arr,int n)throws BSException{
+        int left=0,right=arr.length-1,range=0,i=0;
+        while(true){
+            range = right-left;
+            if(range<0)
+                throw new BSException("Limits reversed");
+            else if(range==0 && arr[left] != n)
+                throw new BSException("no target found");
+            else if( arr[left] > arr[right] ){
+                throw new BSException("Array not sorted");
+            }
+            i = range/2+left;
             if(arr[i]>n){
-                right=i;
+                right=i-1;
             } else if(arr[i]<n){
-                left=i;
+                left=i+1;
             } else {
                 return i;
             }
-            i=((left+right)/2);
         }
-        return -1;
     }
 
-    public static int findTarget2(int[] arr,int left,int right,int n){
-        if(left>right)
-            return -1;
-        int mid = (left+right)/2;
+    public static int findTarget2(int[] arr,int left,int right,int n) throws BSException{
+        int range = right-left;
+        if(range<0)
+            throw new BSException("Limits reversed");
+        else if(range == 0 && arr[left] != n)
+            throw new BSException("no target found");
+        else if( arr[left] > arr[right] ){
+            throw new BSException("Array not sorted");
+        }
+        int mid = range/2+left;
         if(arr[mid]>n){
-            return findTarget2(arr,left,mid,n);
+            return findTarget2(arr,left,mid-1,n);
         } else if(arr[mid]<n){
-            return findTarget2(arr,mid,right,n);
+            return findTarget2(arr,mid+1,right,n);
         } else {
             return mid;
+        }
+    }
+
+    private static class BSException extends Exception {
+
+        public BSException(String message) {
+            super(message);
         }
     }
 }
