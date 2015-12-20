b16c23e9c94c9fcb7df98389c4dbcabefe71b793
alpenliebe <alpseinstein@gmail.com>
2015-12-19 19:03:08 -0500

    Multi-Key Sort employees

--- src/ca/javabeast/algorithms/sorting/Sorting.java
+++ src/ca/javabeast/algorithms/sorting/Sorting.java
@@ -15,6 +15,9 @@
  */
 package ca.javabeast.algorithms.sorting;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  *
  * @author alpenliebe <alpseinstein@gmail.com>
@@ -27,18 +30,19 @@ public class Sorting {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
     public void selectionSort(int[] data) {
-        for(int start=0;start<data.length-1;start++){
+        for (int start = 0; start < data.length - 1; start++) {
             insert(data, start, findMinimumIndex(data, start));
         }
     }
+
     //stable sort
     private void insert(int[] data, int start, int index) {
-        if(index>start){
-            int temp=data[index];
-            System.arraycopy(data, start, data, start+1, index-start);
-            data[start]=temp;
+        if (index > start) {
+            int temp = data[index];
+            System.arraycopy(data, start, data, start + 1, index - start);
+            data[start] = temp;
         }
     }
 
@@ -70,69 +74,148 @@ public class Sorting {
             data[index2] = tmp;
         }
     }
-    
-    public void insertionSort(int[] data){
-        for(int which=1;which<data.length;which++){
+
+    public void insertionSort(int[] data) {
+        for (int which = 1; which < data.length; which++) {
             int val = data[which];
-            for(int i = 0; i < which;i++){
-                if(data[i]>val){
-                    System.arraycopy(data, i, data, i+1, which-i);
-                    data[i]=val;
+            for (int i = 0; i < which; i++) {
+                if (data[i] > val) {
+                    System.arraycopy(data, i, data, i + 1, which - i);
+                    data[i] = val;
                     break;
                 }
             }
         }
     }
-    
-    public void quickSort(int[] data,int left,int right){
-        int range = right-left;
+
+    public void quickSort(int[] data, int left, int right) {
+        int range = right - left;
         int i = left, j = right;
-        int pivot = data[left+range/2];
+        int pivot = data[left + range / 2];
         while (i <= j) {
-            while (data[i]<pivot){
+            while (data[i] < pivot) {
                 i++;
             }
-            while (data[j]<pivot){
+            while (data[j] < pivot) {
                 j--;
             }
             if (i <= j) {
-                  swap(data,i,j);
-                  i++;
-                  j--;
+                swap(data, i, j);
+                i++;
+                j--;
             }
-            
-        }
-        if (left < i - 1)
-            quickSort(data,left,i-1);
-        if (i < right)
-            quickSort(data,i,right);
-    } 
-   
-    public void mergeSort(int[] data,int left,int right){
-        int range = right-left;
-        if(range>0){
-            int mid = left+range/2;
-            mergeSort(data,left,mid);
-            mergeSort(data,mid,right);
-            mergePart(data,left,mid,right);
-        }
-    } 
 
-    private void mergePart(int[] data, int left, int mid, int right) {
-        int[] temp = new int[right+1];
-        System.arraycopy(data, left, temp, left, right-left+1);
-        int i=left,j=mid + 1,k=left;
-        while(i<=mid&&j<=right){
-            if(temp[i]<=temp[j]){
-                data[k]=temp[i++];
-            } else {
-                data[k]=temp[j++];
-            }
-            k++;
         }
-        while(i<=mid){
-            data[k++]=temp[i++];
+        if (left < i - 1) {
+            quickSort(data, left, i - 1);
+        }
+        if (i < right) {
+            quickSort(data, i, right);
         }
     }
 
+    public void mergeSort(int[] data, int left, int right) {
+        int range = right - left;
+        if (range > 0) {
+            int mid = left + range / 2;
+            mergeSort(data, left, mid);
+            mergeSort(data, mid, right);
+            mergePart(data, left, mid, right);
+        }
+    }
+
+    private void mergePart(int[] data, int left, int mid, int right) {
+        int[] temp = new int[right + 1];
+        System.arraycopy(data, left, temp, left, right - left + 1);
+        int i = left, j = mid + 1, k = left;
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                data[k] = temp[i++];
+            } else {
+                data[k] = temp[j++];
+            }
+            k++;
+        }
+        while (i <= mid) {
+            data[k++] = temp[i++];
+        }
+    }
+
+    
+    public void sortEmployees( Employee[] employees ){
+        Comparator<Employee> cp = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                // Compare surnames
+                int ret = e1.surname.compareToIgnoreCase(e2.surname);
+                if (ret == 0) { //Compare givennames if surnames are the same
+                    ret = e1.givenname.compareToIgnoreCase(e2.givenname);
+                }
+                return ret;
+            }
+        };
+
+        Arrays.sort( employees, cp );
+    }
+    public void sort(Employee[] data) {
+        Comparator<Employee> cp = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                // Compare surnames
+                int ret = e1.surname.compareToIgnoreCase(e2.surname);
+                if (ret == 0) { //Compare givennames if surnames are the same
+                    ret = e1.givenname.compareToIgnoreCase(e2.givenname);
+                }
+                return ret;
+            }
+        };
+
+        quickSort(data, cp, 0, data.length - 1);
+    }
+
+    private void quickSort(Employee[] data, Comparator cp, int left, int right) {
+        int range = right - left;
+        int i = left, j = right;
+        Employee pivot = data[left + range / 2];
+        while (true) {
+            while (cp.compare(data[i], pivot) == 1) {
+                i++;
+            }
+            while (cp.compare(data[j], pivot) == -1) {
+                j--;
+            }
+            if (i > j) {
+                break;
+            }
+
+            swap(data, i, j);
+            i++;
+            j--;
+
+        }
+        if (left < i - 1) {
+            quickSort(data, cp, left, i - 1);
+        }
+        if (i < right) {
+            quickSort(data, cp, i, right);
+        }
+    }
+
+    private void swap(Employee[] data, int index1, int index2) {
+        if (index1 != index2) {
+            Employee tmp = data[index1];
+            data[index1] = data[index2];
+            data[index2] = tmp;
+        }
+    }
+
+    private static class Employee {
+
+        public String extension;
+        public String givenname;
+        public String surname;
+
+        public Employee() {
+        }
+    }
 }
