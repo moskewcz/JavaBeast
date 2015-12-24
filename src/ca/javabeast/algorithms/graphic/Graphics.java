596e4632acc078644cddc11ea77dcfcf10cbc675
alpenliebe <alpseinstein@gmail.com>
2015-12-23 20:24:19 -0500

    ....

--- src/ca/javabeast/algorithms/graphic/Graphics.java
+++ src/ca/javabeast/algorithms/graphic/Graphics.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ca.javabeast.algorithms.graphics;
+package ca.javabeast.algorithms.graphic;
 
 /**
  *
