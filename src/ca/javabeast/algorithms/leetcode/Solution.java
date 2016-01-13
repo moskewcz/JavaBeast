2825a175cf965f3f1ddd7fc105f06086443095ad
alpenliebe <alpseinstein@gmail.com>
2016-01-13 18:58:59 -0500

    76. Minimum Window Substring

--- src/ca/javabeast/algorithms/leetcode/Solution.java
+++ src/ca/javabeast/algorithms/leetcode/Solution.java
@@ -857,6 +857,7 @@ class LeetCode {
         }
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
+
     public int minSubArrayLen2(int s, int[] nums) {
         int[] sums = new int[nums.length + 1];
         for (int i = 1; i < nums.length + 1; i++) {
@@ -874,6 +875,7 @@ class LeetCode {
         }
         return min == sums.length + 1 ? 0 : min;
     }
+
     int binarySearch(int[] sums, int v, int left, int right) {
         while (left <= right) {
             int mid = (left + right) / 2;
@@ -884,5 +886,53 @@ class LeetCode {
             }
         }
         return left;
+    }
+
+    //76. Minimum Window Substring
+    public String minWindow(String s, String t) {
+        if (t.length() <= 0 || s.length() < t.length()) {
+            return "";
+        }
+        int start = 0, end = 0, i = 0, j = 0, count = t.length(), min = s.length() + 1;
+        int[] table = new int[256];
+
+        for (int k = 0; k < count; k++) {
+            char c = t.charAt(k);
+            table[c]++;
+        }
+        for (int k = 0; k < 256; k++) {
+            if (table[k] < 1) {
+                table[k] = Integer.MIN_VALUE;
+            }
+        }
+        while (end < s.length()) {
+            while (end < s.length() && count > 0) {
+                char c = s.charAt(end++);
+                if (table[c] != Integer.MIN_VALUE) {
+                    if (table[c] > 0) {
+                        count--;
+                    }
+                    table[c]--;
+                }
+            }
+            if (count > 0) {
+                break;
+            }
+            while (start < s.length() && count <= 0) {
+                char c = s.charAt(start++);
+                if (table[c] != Integer.MIN_VALUE) {
+                    if (table[c] >= 0) {
+                        count++;
+                    }
+                    table[c]++;
+                }
+            }
+            if (end - start + 1 < min) {
+                min = end - start + 1;
+                i = start - 1;
+                j = end;
+            }
+        }
+        return min == s.length() + 1 ? "" : s.substring(i, j);
     }
 }
