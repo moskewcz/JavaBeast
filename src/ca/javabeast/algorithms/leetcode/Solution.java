64292ea4934b7339679dbfd5a324f16e5b0a9ff1
alpenliebe <alpseinstein@gmail.com>
2016-02-23 23:15:06 -0500

    10. Regular Expression Matching

--- src/ca/javabeast/algorithms/leetcode/Solution.java
+++ src/ca/javabeast/algorithms/leetcode/Solution.java
@@ -1291,6 +1291,24 @@ class LeetCode {
         }
     }
 
+    //10. Regular Expression Matching
+    public boolean isMatch(String s, String p) {
+        int sl = s.length(), pl = p.length();
+        boolean[][] dp = new boolean[sl + 1][pl + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= sl; i++) {
+            for (int j = 1; j <= pl; j++) {
+                char c = p.charAt(j - 1);
+                if (c == '*') {
+                    dp[i][j] = (j > 1 && dp[i][j - 2]) || (i > 0 && dp[i - 1][j] && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)));
+                } else {
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (c == '.' || c == s.charAt(i - 1));
+                }
+            }
+        }
+        return dp[sl][pl];
+    }
+
     //
     //G questions 1: for all i in  i .. n sum(i/3+i/5)
     public int sumDividend(int n) {
