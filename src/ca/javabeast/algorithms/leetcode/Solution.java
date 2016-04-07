0326161a81ad192601ed20d2c702cf5e6f334167
alpenliebe <alpseinstein@gmail.com>
2016-04-07 01:11:59 -0400

    15. 3Sum

--- src/ca/javabeast/algorithms/leetcode/Solution.java
+++ src/ca/javabeast/algorithms/leetcode/Solution.java
@@ -2598,7 +2598,6 @@ class LeetCode {
     }
 
     //268. Missing Number
-
     public int missingNumber(int[] nums) {
         if (nums == null || nums.length < 1) {
             return 0;
@@ -2843,7 +2842,6 @@ class LeetCode {
         }
 
         // Add the number to an internal data structure.
-
         public void add(int number) {
             Boolean flag = nums.get(number);
             if (flag != null) {
@@ -3287,6 +3285,39 @@ class LeetCode {
             }
         }
         return stack.size() == 0;
+    }
+
+    //15. 3Sum
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 2 && nums[i] <= 0; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1, right = n - 1;
+                while (left < right) {
+                    if (nums[left] + nums[right] > -nums[i]) {
+                        right--;
+                    } else if (nums[left] + nums[right] < -nums[i]) {
+                        left++;
+                    } else {
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
     }
 
     //8. String to Integer (atoi)
