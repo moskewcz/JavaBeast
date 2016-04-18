79ff3860b0af431de3eebf4809c63af24219f185
Alpenliebe <alpseinstein@gmail.com>
2016-04-18 14:31:17 -0400

    162. Find Peak Element

--- src/ca/javabeast/algorithms/leetcode/Solution.java
+++ src/ca/javabeast/algorithms/leetcode/Solution.java
@@ -3258,6 +3258,20 @@ class LeetCode {
 
     }
 
+    //162. Find Peak Element
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] < nums[m + 1]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return l;
+    }
+
     //160. Intersection of Two Linked Lists
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode meet = null, cur = headA, slow = headB, fast = headB;
