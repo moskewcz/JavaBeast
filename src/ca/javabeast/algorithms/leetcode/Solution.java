aa89fd9e0676bef33da3d7d4ef025b9cef392a96
Alpenliebe <alpseinstein@gmail.com>
2016-05-08 19:53:05 -0400

    341. Flatten Nested List Iterator

--- src/ca/javabeast/algorithms/leetcode/Solution.java
+++ src/ca/javabeast/algorithms/leetcode/Solution.java
@@ -1931,6 +1931,53 @@ class LeetCode {
     }
 
     //M/E
+    public interface NestedInteger {
+
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        public boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return null if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+
+    //341. Flatten Nested List Iterator
+    public class NestedIterator implements Iterator<Integer> {
+
+        Deque<NestedInteger> stack;
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            stack = new ArrayDeque<>();
+            for (int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.push(nestedList.get(i));
+            }
+        }
+
+        @Override
+        public Integer next() {
+            return stack.pop().getInteger();
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (stack.size() > 0) {
+                NestedInteger cur = stack.peek();
+                if (cur.isInteger()) {
+                    return true;
+                }
+                stack.pop();
+                for (int i = cur.getList().size() - 1; i >= 0; i--) {
+                    stack.push(cur.getList().get(i));
+                }
+            }
+            return false;
+        }
+    }
+
     //336. Palindrome Pairs
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> res = new ArrayList<>();
