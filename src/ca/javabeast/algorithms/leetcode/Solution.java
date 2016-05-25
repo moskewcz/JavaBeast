bd5b59705b9ff991782ce9020611da7fab3d2f65
Alpenliebe <alpseinstein@gmail.com>
2016-05-25 14:22:16 -0400

    169. Majority Element

--- src/ca/javabeast/algorithms/leetcode/Solution.java
+++ src/ca/javabeast/algorithms/leetcode/Solution.java
@@ -4293,6 +4293,22 @@ class LeetCode {
         }
     }
 
+    //169. Majority Element
+    public int majorityElement(int[] nums) {
+        int res = nums[0], count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count <= 0) {
+                count++;
+                res = nums[i];
+            } else if (res == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return res;
+    }
+
     //168. Excel Sheet Column Title
     public String convertToTitle(int n) {
         StringBuilder sb = new StringBuilder();
